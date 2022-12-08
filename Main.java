1678d7eb9c2da44d7ba7e14f26b050a2ca55fc8e
harveyhyena21 <ohu02@ladueschools.net>
2022-12-08 19:47:48 +0000

    added to all method englishToArray and code method are working as intended

--- Main.java
+++ Main.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+    //the latin and morse code alphabet in arrays
+    private static final String[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+    private static final String[] morseAlphabet = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+            "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." }; 
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String answer = "";
 
-        // continues looping until user provides the letter 'c' for coding or 'd' for
-        // decoding
+        // continues looping until user provides the letter 'c' for coding or 'd' 
+        // for decoding
         boolean isValid = false;
         while (!isValid) {
             System.out.println("Would you like to (c)ode or (d)ecode Morse code?");
@@ -15,14 +21,17 @@ public class Main {
 
             if (answer.equals("c")) {
                 isValid = true;
-                System.out.println("Enter the phrase you wish to code:");
+                System.out.println("\nEnter the phrase you wish to code (no punctuation):");
                 ArrayList<String> phrase = englishToArray(sc.nextLine());
                 System.out.println(code(phrase));
+
             } else if (answer.equals("d")) {
                 isValid = true;
-                System.out.println("Enter the phrase you wish to decode:");
+                System.out.println("\nEnter the phrase you wish to decode (no punctuation):");
                 ArrayList<String> phrase = morseToArray(sc.nextLine());
+                System.out.println(phrase);
                 System.out.println(decode(phrase));
+
             } else
                 System.out.println("\nInvalid input. Please try again.");
         }
@@ -30,27 +39,65 @@ public class Main {
         sc.close();
     }
 
-    //converts an English phrase to an ArrayList
-    public static ArrayList<String> englishToArray(String s) {
+    // converts an English phrase to an ArrayList with each element being one word
+    public static ArrayList<String> englishToArray(String phrase) {
         ArrayList<String> output = new ArrayList<String>();
+
+        boolean stop = false;
+    
+        while (!stop) {
+          output.add(phrase.substring(0, phrase.indexOf(" ")));
+          phrase = phrase.substring(phrase.indexOf(" ") + 1);
+          if (phrase.indexOf(" ") == -1) {
+            output.add(phrase);
+            stop = true;
+          }
+        }
 
         return output;
     }
 
-    //converts a Morse code phrase to an ArrayList
-    public static ArrayList<String> morseToArray(String s) {
+    // converts a Morse code phrase to an ArrayList with each element being one word
+    public static ArrayList<String> morseToArray(String phrase) {
         ArrayList<String> output = new ArrayList<String>();
+
+        boolean stop = false;
+    
+        while (!stop) {
+          output.add(phrase.substring(0, phrase.indexOf("/")));
+          phrase = phrase.substring(phrase.indexOf("/") + 1);
+          if (phrase.indexOf("/") == -1) {
+            output.add(phrase);
+            stop = true;
+          }
+        }
 
         return output;
     }
 
-    public static String code(ArrayList<String> s) {
+    // codes the phrase
+    public static String code(ArrayList<String> phrase) {
         String output = "";
+
+        for (int i = 0; i < phrase.size(); i++) {
+            for (int j = 0; j < phrase.get(i).length(); j++) {
+                for (int k = 0; k < alphabet.length; k++) {
+                    if (phrase.get(i).substring(j, j + 1).equals(alphabet[k])) output += morseAlphabet[k];
+                }
+                output += " ";
+            }
+            if (i != phrase.size() - 1) output += "/ ";
+        }
+
         return output;
     }
 
+    // decodes the phrase
     public static String decode(ArrayList<String> s) {
         String output = "";
+
+
+
         return output;
     }
 }
