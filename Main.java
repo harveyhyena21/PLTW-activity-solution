a8dc2808fea668ada65cf2175f33dd9c40fa71bd
harveyhyena21 <ohu02@ladueschools.net>
2022-12-08 15:10:55 +0000

    added methods for toArray and

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String answer = "";
 
-        //continues looping until user provides the letter 'c' for coding or 'd' for decoding
+        // continues looping until user provides the letter 'c' for coding or 'd' for
+        // decoding
         boolean isValid = false;
         while (!isValid) {
             System.out.println("Would you like to (c)ode or (d)ecode Morse code?");
@@ -14,27 +16,41 @@ public class Main {
             if (answer.equals("c")) {
                 isValid = true;
                 System.out.println("Enter the phrase you wish to code:");
-                code();
-            }
-            else if (answer.equals("d")) {
+                ArrayList<String> phrase = englishToArray(sc.nextLine());
+                System.out.println(code(phrase));
+            } else if (answer.equals("d")) {
                 isValid = true;
                 System.out.println("Enter the phrase you wish to decode:");
-            }
-            else System.out.println("\nInvalid input. Please try again.");
+                ArrayList<String> phrase = morseToArray(sc.nextLine());
+                System.out.println(decode(phrase));
+            } else
+                System.out.println("\nInvalid input. Please try again.");
         }
-        
+
         sc.close();
     }
 
-    public static void toArray() {
+    //converts an English phrase to an ArrayList
+    public static ArrayList<String> englishToArray(String s) {
+        ArrayList<String> output = new ArrayList<String>();
 
+        return output;
     }
 
-    public static void code() {
+    //converts a Morse code phrase to an ArrayList
+    public static ArrayList<String> morseToArray(String s) {
+        ArrayList<String> output = new ArrayList<String>();
 
+        return output;
     }
 
-    public static void decode() {
+    public static String code(ArrayList<String> s) {
+        String output = "";
+        return output;
+    }
 
+    public static String decode(ArrayList<String> s) {
+        String output = "";
+        return output;
     }
 }
