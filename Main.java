2807e0b934c68eb7796dbfaeb3e898862275fa41
harveyhyena21 <ohu02@ladueschools.net>
2022-12-09 16:16:57 +0000

    worked on decode method, not yet complete

--- Main.java
+++ Main.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    //the latin and morse code alphabet in arrays
-    private static final String[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-    private static final String[] morseAlphabet = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
-            "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." }; 
+    // the latin and morse code alphabet in arrays
+    private static final String[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+    private static final String[] morseAlphabet = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+            ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+            "-.--", "--.." };
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String answer = "";
 
-        // continues looping until user provides the letter 'c' for coding or 'd' 
+        // continues looping until user provides the letter 'c' for coding or 'd'
         // for decoding
         boolean isValid = false;
         while (!isValid) {
@@ -29,7 +30,7 @@ public class Main {
                 isValid = true;
                 System.out.println("\nEnter the phrase you wish to decode (no punctuation):");
                 ArrayList<String> phrase = morseToArray(sc.nextLine());
-                System.out.println(phrase);
+                //System.out.println(phrase);
                 System.out.println(decode(phrase));
 
             } else
@@ -44,14 +45,14 @@ public class Main {
         ArrayList<String> output = new ArrayList<String>();
 
         boolean stop = false;
-    
+
         while (!stop) {
-          output.add(phrase.substring(0, phrase.indexOf(" ")));
-          phrase = phrase.substring(phrase.indexOf(" ") + 1);
-          if (phrase.indexOf(" ") == -1) {
-            output.add(phrase);
-            stop = true;
-          }
+            output.add(phrase.substring(0, phrase.indexOf(" ")));
+            phrase = phrase.substring(phrase.indexOf(" ") + 1);
+            if (phrase.indexOf(" ") == -1) {
+                output.add(phrase);
+                stop = true;
+            }
         }
 
         return output;
@@ -62,14 +63,17 @@ public class Main {
         ArrayList<String> output = new ArrayList<String>();
 
         boolean stop = false;
-    
+
         while (!stop) {
-          output.add(phrase.substring(0, phrase.indexOf("/")));
-          phrase = phrase.substring(phrase.indexOf("/") + 1);
-          if (phrase.indexOf("/") == -1) {
-            output.add(phrase);
-            stop = true;
-          }
+            
+            output.add(phrase.substring(0, phrase.indexOf(" /")));
+            phrase = phrase.substring(phrase.indexOf(" /") + 3);
+            if (phrase.indexOf(" /") == -1) {
+                output.add(phrase);
+                stop = true;
+            } else {
+                
+            }
         }
 
         return output;
@@ -82,21 +86,49 @@ public class Main {
         for (int i = 0; i < phrase.size(); i++) {
             for (int j = 0; j < phrase.get(i).length(); j++) {
                 for (int k = 0; k < alphabet.length; k++) {
-                    if (phrase.get(i).substring(j, j + 1).equals(alphabet[k])) output += morseAlphabet[k];
+                    if (phrase.get(i).substring(j, j + 1).equals(alphabet[k]))
+                        output += morseAlphabet[k];
                 }
                 output += " ";
             }
-            if (i != phrase.size() - 1) output += "/ ";
+            if (i != phrase.size() - 1)
+                output += "/ ";
         }
 
         return output;
     }
 
     // decodes the phrase
-    public static String decode(ArrayList<String> s) {
+    public static String decode(ArrayList<String> phrase) {
         String output = "";
 
+        ArrayList<String> letter = new ArrayList<String>();
+        boolean stop = false;
 
+        for (int i = 0; i < phrase.size(); i++) {
+            letter.clear();
+
+            stop = false;
+            while (!stop) {
+                letter.add(phrase.get(i).substring(0, phrase.get(i).indexOf(" ")));
+                phrase.set(i, phrase.get(i).substring(phrase.get(i).indexOf(" ") + 1));
+                if (phrase.indexOf(" ") == -1) {
+                    letter.add(phrase.get(i));
+                    stop = true;
+                }
+            }
+
+            System.out.println(letter);
+            for (int j = 0; j < letter.size(); j++) {
+                for (int k = 0; k < morseAlphabet.length; k++) {
+                    if (letter.get(j).equals(morseAlphabet[k]))
+                        output += alphabet[k];
+                
+                }
+            }
+            
+            output += " ";
+        }
 
         return output;
     }
