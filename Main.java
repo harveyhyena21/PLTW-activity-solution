751a9fbaec206d36dae7fd1de4aa639db393cbf3
harveyhyena21 <ohu02@ladueschools.net>
2022-12-11 20:49:35 +0000

    ...............

--- Main.java
+++ Main.java
@@ -1,8 +1,4 @@
 /* TO DO (in order)
- * check if phrase inputted is correct for decoding or coding (e.g. not putting latin alphabet characters for decoding part)
- * tells user more (like to include slashes and spaces and what to use for dots and dashes)
- * add punctuation to arrays (maybe even numbers?)
- * if punctuation is added, would have to adjust code so spaces aren't added before the punctuation
  * edit to create activity obvi
  * write steps for the activity and hints maybe?
  */
@@ -30,42 +26,44 @@ public class Main {
             // for decoding
             boolean isValid = false;
             while (!isValid) {
-                System.out.println("\nWould you like to (c)ode or (d)ecode Morse code?");
+                System.out.println("\n\033[1;37mWould you like to (c)ode or (d)ecode Morse code?\033[0m");
                 answer = sc.nextLine();
 
                 if (answer.equals("c")) {
                     isValid = true;
-                    System.out.println("\nEnter the phrase you wish to code (no punctuation):");
+                    System.out.println("\n\033[1;37m\033[4;37mEnter the phrase you wish to code (no punctuation):\033[0m");
+
                     // converts the phrase into an ArrayList separated by words
                     ArrayList<String> phrase = englishToArray(sc.nextLine());
-                    System.out.println("\nYour coded phrase:\n" + code(phrase));
+                    System.out.println("\n\033[1;37m\033[4;37mYour coded phrase:\033[0m\n" + code(phrase));
 
                 } else if (answer.equals("d")) {
                     isValid = true;
-                    System.out.println("\nEnter the phrase you wish to decode (no punctuation):");
+                    System.out.println("\n\033[1;37m\033[4;37mEnter the phrase you wish to decode (no punctuation):\033[0m");
+                    System.out.println("\033[0;30mPlease use periods (\".\") for dots and hyphens (\"-\") for dashes. Separate letters with spaces and words with a forward slash with a space on both sides (\" / \"). Example: .... . .-.. .-.. --- / .-- --- .-. .-.. -.. which translates into \"hello world.\"\033[0m");
+
                     // converts the phrase into an ArrayList separated by slashes (words)
                     ArrayList<String> phrase = morseToArray(sc.nextLine());
-                    // System.out.println(phrase);
-                    System.out.println("\nYour decoded phrase:\n" + decode(phrase));
+                    System.out.println("\n\033[1;37m\033[4;37mYour decoded phrase:\033[0m\n" + decode(phrase));
 
                 } else
-                    System.out.println("\nInvalid input. Please try again.");
+                    System.out.println("\n\033[1;37mInvalid input. Please try again.\033[0m");
             }
 
             isValid = false;
 
             // repeats while the user enters a valid input
             while (!isValid) {
-                System.out.println("\nWould you like to code or decode something else? (Y)es or (n)o?");
+                System.out.println("\n\033[1;37mWould you like to code or decode something else? (Y)es or (n)o?\033[0m");
                 answer = sc.nextLine();
                 if (answer.toLowerCase().equals("n")) {
                     again = false;
                     isValid = true;
-                    System.out.println("\nThanks for using this Morse code coder/decoder!");
+                    System.out.println("\n\033[1;37mThanks for using this Morse code coder/decoder!\033[0m");
                 } else if (answer.toLowerCase().equals("y"))
                     isValid = true;
                 else
-                    System.out.println("\nInvalid input. Please try again.");
+                    System.out.println("\n\033[1;37mInvalid input. Please try again.\033[0m");
             }
         }
 
@@ -76,11 +74,13 @@ public class Main {
     public static ArrayList<String> englishToArray(String phrase) {
         ArrayList<String> output = new ArrayList<String>();
 
-        boolean stop = false;
+        phrase = phrase.toLowerCase();
 
+        boolean stop = false;
         // repeats through the entire phrase word by word
         while (!stop) {
-            // if on the last word, adds the remaining phrase string; to prevent the off by one problem
+            // if on the last word or there's only one word in the phrase, adds the
+            // remaining phrase string, to prevent the off by one problem
             // else, adds the word to the ArrayList, then deletes the word from the phrase
             if (phrase.indexOf(" ") == -1) {
                 output.add(phrase);
@@ -102,7 +102,8 @@ public class Main {
 
         // repeats through the entire phrase word by word
         while (!stop) {
-            // if on the last word, adds the remaining phrase string; to prevent the off by one problem
+            // if on the last word or there's only one word in the phrase, adds the
+            // remaining phrase string, to prevent the off by one problem
             // else, adds the word to the ArrayList, then deletes the word from the phrase
             if (phrase.indexOf(" /") == -1) {
                 output.add(phrase);
@@ -120,11 +121,11 @@ public class Main {
     public static String code(ArrayList<String> phrase) {
         String output = "";
 
-        //loops through each word
+        // loops through each word
         for (int i = 0; i < phrase.size(); i++) {
-            //loops through each letter
+            // loops through each letter
             for (int j = 0; j < phrase.get(i).length(); j++) {
-                // finds the morse sequence that matches the latin alphabet letter and adds it 
+                // finds the morse sequence that matches the latin alphabet letter and adds it
                 // to the output string
                 for (int k = 0; k < alphabet.length; k++) {
                     if (phrase.get(i).substring(j, j + 1).equals(alphabet[k]))
@@ -144,32 +145,33 @@ public class Main {
     // decodes the phrase
     public static String decode(ArrayList<String> phrase) {
         String output = "";
-        
-        //new ArrayList that will contain each word, separated by each letter
+
+        // new ArrayList that will contain each word, separated by each letter
         ArrayList<String> letter = new ArrayList<String>();
         boolean stop = false;
 
-        //loops through each word
+        // loops through each word
         for (int i = 0; i < phrase.size(); i++) {
             letter.clear();
 
-            //utilizes the same function as "toArray" methods to add to the letter ArrayLists
+            // utilizes the same function as "toArray" methods to add to the letter ArrayLists
             stop = false;
             while (!stop) {
-                letter.add(phrase.get(i).substring(0, phrase.get(i).indexOf(" ")));
-                phrase.set(i, phrase.get(i).substring(phrase.get(i).indexOf(" ") + 1));
                 if (phrase.get(i).indexOf(" ") == -1) {
                     letter.add(phrase.get(i));
                     stop = true;
+                } else {
+                    letter.add(phrase.get(i).substring(0, phrase.get(i).indexOf(" ")));
+                    phrase.set(i, phrase.get(i).substring(phrase.get(i).indexOf(" ") + 1));
                 }
             }
 
             // uncomment to debug your code
             // System.out.println(letter);
 
-            //loops through each letter
+            // loops through each letter
             for (int j = 0; j < letter.size(); j++) {
-                // finds the latin alphabet letter that matches the morse sequence and adds it 
+                // finds the latin alphabet letter that matches the morse sequence and adds it
                 // to the output string
                 for (int k = 0; k < morseAlphabet.length; k++) {
                     if (letter.get(j).equals(morseAlphabet[k]))
@@ -177,8 +179,8 @@ public class Main {
 
                 }
             }
-            
-            //separates words by a space
+
+            // separates words by a space
             output += " ";
         }
 
